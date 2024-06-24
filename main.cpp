d94e457899204f283f8a9644814ceee7991a3db1
toastoffee <931391442@qq.com>
2024-06-24 19:36:08 +0800

    add example

--- main.cpp
+++ main.cpp
@@ -4,6 +4,25 @@
 
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+    Socket clientSocket(AddressFamily::InterNetwork, SocketType::Stream, ProtocolType::Tcp);
+
+    // connect
+    clientSocket.Connect("127.0.0.1", 8080);
+
+    while(true){
+        std::string msg;
+        std::cin >> msg;
+
+        // send
+        clientSocket.Send(msg.c_str(), strlen(msg.c_str()), SocketFlags::None);
+
+        // receive
+        char buffer[1024] = {0};
+        clientSocket.Receive(buffer, sizeof(buffer), SocketFlags::None);
+
+        std::cout << "[msg receive from server] " << buffer << std::endl;
+    }
+
     return 0;
 }
