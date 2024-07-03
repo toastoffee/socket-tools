1ebb9cf0c55d7f4f1d3f1e9a10287c5faa25cb06
toastoffee <931391442@qq.com>
2024-07-03 17:23:37 +0800

    fix the select example

--- examples/select_chatroom/server/main.cpp
+++ examples/select_chatroom/server/main.cpp
@@ -21,80 +21,6 @@
 
 #define MAX_SIZE 1024
 
-//int main() {
-//
-//    // listen Socket
-//    Socket listenSocket(AddressFamily::InterNetwork, SocketType::Stream, ProtocolType::Tcp);
-//
-//    int maxFd = -1;
-//    fd_set readFds;
-//    char msg[MAX_SIZE];
-//
-//    int allfds[MAX_SIZE];
-//    memset(allfds, -1, sizeof(allfds));
-//    Socket* conn;
-//
-//    int index = 0;
-//
-//    // bind
-//    listenSocket.Bind("127.0.0.1", 8888);
-//
-//    // listen
-//    listenSocket.Listen(0);
-//
-//    allfds[index] = maxFd = listenSocket.GetFd();
-//
-//    std::cout << "[server] chatroom start successfully" << std::endl;
-//
-//    // waiting...
-//    while(true){
-//        FD_ZERO(&readFds);
-//
-//        for(int i = 0; i <= index; i++){
-//            FD_SET(allfds[i], &readFds);
-//        }
-//
-//        int nCount = select(maxFd+1, &readFds, NULL, NULL, NULL);
-//
-//        for(int i = 0; i < MAX_SIZE; i++){
-//            if(nCount == 0){
-//                break;
-//            }
-//            if(!FD_ISSET(allfds[i], &readFds)){
-//                continue;
-//            }
-//
-//            // listen that client has connected
-//            if(allfds[i] == listenSocket.GetFd()) {
-//                nCount--;
-//                conn = listenSocket.Accept();
-//
-//                for (int n = 0; n < MAX_SIZE; n++) {
-//                    if (allfds[n] == -1) {
-//                        allfds[n] = conn->GetFd();
-//                        maxFd = maxFd > conn->GetFd() ? maxFd : conn->GetFd();
-//                        index = index > n ? index : n;
-//                        break;
-//                    }
-//                }
-//            }
-//            else{   // listen that client has sent msg
-//                memset(msg, 0, sizeof(msg));
-//                recv(allfds[i], msg, MAX_SIZE, 0);
-//
-//                std::cout << msg << std::endl;
-//
-//                send(allfds[i], msg, MAX_SIZE, 0);
-//            }
-//        }
-//
-//    }
-//
-//    listenSocket.Close();
-//
-//    return 0;
-//}
-
 int main() {
 
     // initial variables
@@ -146,9 +72,28 @@ int main() {
                 nCount--;
 
                 memset(msg, 0, sizeof(msg));
-                socket->Receive(msg, MAX_SIZE);
-                std::cout <<"[server] msg from client:" << msg << std::endl;
-                socket->Send(msg, MAX_SIZE);
+                int recvSize = socket->Receive(msg, MAX_SIZE);
+
+                if(recvSize == 0){
+                    // the client has disconnected
+                    std::cout <<"[server] client-"<< socket->GetFd() << " has disconnected:" << msg << std::endl;
+                    for(auto it = sockets.begin(); it != sockets.end(); ++it){
+                        if(*it == socket){
+                            sockets.erase(it);
+                            break;
+                        }
+                    }
+                    socket->Close();
+                }else{
+                    std::cout <<"[server] msg from client:" << msg << std::endl;
+
+                    for (auto client : sockets) {
+                        if(client->GetFd() != listenSocket.GetFd()){
+                            std::cout <<"[server] msg sent to client-" << client->GetFd();
+                            client->Send(msg, MAX_SIZE);
+                        }
+                    }
+                }
             }
         }
     }
